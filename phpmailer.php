7cb978ea0225877a4fb8ff4d4f2ac1d424e519e3
Andrea Geraldinne Laruta Coronel <andreageraldinne@gmail.com>
2024-09-25 14:16:49 -0400

    adding phpmailer

---
+++ phpmailer.php
@@ -0,0 +1,42 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] != 'POST' ){
+    header("Location: index.html" );
+}
+
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+$nombre = $_POST['name'];
+$email = $_POST['email_address'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+if( empty(trim($nombre)) ) $nombre = 'anonimo';
+if( empty(trim($email_address)) ) $email_address = '';
+if( empty(trim($phone)) ) $phone = '';
+
+$body = <<<HTML
+    <h1>Contacto desde la web</h1>
+    <p>De: $name</p>
+    <p>phone: $phone</p>
+    <p>mail: $email_address</p>
+    <h2>Mensaje</h2>
+    $message
+HTML;
+
+$mailer = new PHPMailer();
+$mailer->setFrom( $email, "$name $phone" );
+$mailer->addAddress('servicio@superstarcleaningservices.com','Mensaje del Sitio web');
+$mailer->Subject = "Mensaje web";
+$mailer->msgHTML($body);
+$mailer->AltBody = strip_tags($body);
+$mailer->CharSet = 'UTF-8';
+
+
+
+$rta = $mailer->send( );
+
+var_dump($rta);
+//header("Location: gracias.html" );
